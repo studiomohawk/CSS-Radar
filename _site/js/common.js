ae33354c7330d8b52e986d04a1caa1ffa59baddd
Yuya Saito <studiomohawk@gmail.com>
2011-08-28 15:51:33 +0900

    Lets experiment on Google +1 see if any impact

--- _site/js/common.js
+++ _site/js/common.js
@@ -11,3 +11,9 @@ $(document).ready(function() {
 	});
 // END
 });
+
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
