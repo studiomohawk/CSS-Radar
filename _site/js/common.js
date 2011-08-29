f993e1356ac0e4b4aed64e123b3378c9c7422387
Yuya Saito <studiomohawk@gmail.com>
2011-08-30 00:01:03 +0900

    li was not closed

--- _site/js/common.js
+++ _site/js/common.js
@@ -13,14 +13,14 @@ $(document).ready(function() {
 });
 
 (function() {
-  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-  po.src = 'https://apis.google.com/js/plusone.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	po.src = 'https://apis.google.com/js/plusone.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 })();
 (function(){
-  var twitterWidgets = document.createElement('script');
-  twitterWidgets.type = 'text/javascript';
-  twitterWidgets.async = true;
-  twitterWidgets.src = 'http://platform.twitter.com/widgets.js';
-  document.getElementsByTagName('head')[0].appendChild(twitterWidgets);
+	var twitterWidgets = document.createElement('script');
+	twitterWidgets.type = 'text/javascript';
+	twitterWidgets.async = true;
+	twitterWidgets.src = 'http://platform.twitter.com/widgets.js';
+	document.getElementsByTagName('head')[0].appendChild(twitterWidgets);
 })();
